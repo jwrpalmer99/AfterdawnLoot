7d89c82e494223ff27507a9bcbe5a9a8b403bef9
Jeremy Palmer <jeremyp@outlook.com>
2021-03-27 12:35:16 +0000

    initial commit

---
+++ AfterdawnLoot/Services/EmailSender.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace WebPWrecover.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SendGridClient("***REMOVED***");
+            var from = new EmailAddress("jeremyp@outlook.com");
+
+            var to = new EmailAddress(email);
+            var plainTextContent = message;
+            var htmlContent = message;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            return client.SendEmailAsync(msg);
+        }
+
+    }
+}
