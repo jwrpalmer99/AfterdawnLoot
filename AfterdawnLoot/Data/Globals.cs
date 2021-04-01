7a80ca258e82c948c2483ffdf1fe7cf9e77869b3
Jeremy Palmer <jeremyp@outlook.com>
2021-04-01 13:22:54 +0100

    add cache of pointscores and reset it when changes are saved to db. fix adjustments showing up multiple times in calendar

--- AfterdawnLoot/Data/Globals.cs
+++ AfterdawnLoot/Data/Globals.cs
@@ -11,7 +11,7 @@ public class Globals
     public static long UserID;
     public static List<LootResults> global_lootresults;
     public static List<PointsAdjustment> global_pointadjustments;
-
+    public static List<PlayerCharacterScores> playerscores = new List<PlayerCharacterScores> { };
     public static List<string> classes = new List<string> { "DeathKnight", "DemonHunter", "Druid", "Hunter", "Mage", "Monk", "Paladin", "Priest", "Rogue", "Shaman", "Warlock", "Warrior" };
 }
 
