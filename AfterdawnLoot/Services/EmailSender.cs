33c1192a84c5dc9b15dd6bb2621c78a7fdff96ca
Jeremy Palmer <jeremyp@outlook.com>
2021-04-03 10:15:55 +0100

    readd EmailSender.cs

---
+++ AfterdawnLoot/Services/EmailSender.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace WebPWrecover.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
