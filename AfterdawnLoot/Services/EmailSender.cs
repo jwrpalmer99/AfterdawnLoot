4c68f76defb23b11b4e2d5bef992e29dab06314b
Jeremy Palmer <jeremyp@outlook.com>
2021-04-03 09:38:09 +0100

    remove code from sendemail

--- AfterdawnLoot/Services/EmailSender.cs
+++ AfterdawnLoot/Services/EmailSender.cs
@@ -17,14 +17,7 @@ namespace WebPWrecover.Services
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            var client = new SendGridClient("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");
-            var from = new EmailAddress("jeremyp@outlook.com");
-
-            var to = new EmailAddress(email);
-            var plainTextContent = message;
-            var htmlContent = message;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            return client.SendEmailAsync(msg);
+            return Task.CompletedTask;
         }
 
     }
